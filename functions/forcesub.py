c3eab2416b08f390ec5e3ba33a8a869a19b4d7fd
FilmyFather <91774433+FilmyFather@users.noreply.github.com>
2022-07-21 02:04:26 +0530

    Update forcesub.py

--- functions/forcesub.py
+++ functions/forcesub.py
@@ -39,7 +39,7 @@ async def handle_force_subscribe(bot, message):
     except Exception:
         await bot.send_message(
             chat_id=message.from_user.id,
-            text="Something Went Wrong. Contact My [Support Group](https://t.me/ShadowsArena).",
+            text="Something Went Wrong. Contact My [Support Group](https://t.me/Yuvi_4502).",
             parse_mode="markdown",
             disable_web_page_preview=True,
             reply_to_message_id=message.message_id,
